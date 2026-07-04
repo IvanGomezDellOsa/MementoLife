3b05921f3e481bdbd386197ae8ec5430f1d8476c
Ivan Gomez DellOsa <ivangomezdellosa@gmail.com>
2026-07-04 00:40:18 -0300

    Actualizar toolchain de Android, CI y tokens de diseño

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.application")
     kotlin("android")
@@ -21,10 +23,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     sourceSets {
         getByName("main") {
             kotlin.srcDirs("src/main/kotlin")
@@ -35,6 +33,12 @@ android {
         release {
             isMinifyEnabled = false
         }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
     }
 }
 
